c04fc43ace15129f1c8c18dd39b6708201b3c390
Boffee <zhenbangli@gmail.com>
2024-04-07 00:03:50 -0500

    feat: flatten PoolInfo

--- src/RewardPool.sol
+++ src/RewardPool.sol
@@ -9,16 +9,12 @@ import {IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /// @author Modified from MasterChef V2 (https://github.com/1coinswap/core/blob/master/contracts/MasterChefV2.sol)
 /// @notice This contract is used to manage reward pool.
 contract RewardPool is ERC20 {
-    struct PoolInfo {
-        uint256 emissionRate; // total pool emission per second
-        uint256 emissionPerShare; // total accumulated reward emitted per share
-        uint64 lastUpdatedTimestamp; // last updated timestamp of the pool
-    }
-
     IERC20 public immutable stakeToken;
     IERC20 public immutable rewardToken;
 
-    PoolInfo internal _poolInfo;
+    uint256 public emissionRate; // total pool emission per second
+    uint256 public emissionPerShare; // total accumulated reward emitted per share
+    uint64 public lastUpdatedTimestamp; // last updated timestamp of the pool
 
     mapping(address => int256) internal _debts;
 
@@ -27,45 +23,28 @@ contract RewardPool is ERC20 {
     {
         stakeToken = IERC20(_stakeToken);
         rewardToken = IERC20(_rewardToken);
-        _poolInfo.lastUpdatedTimestamp = uint64(block.timestamp);
+        lastUpdatedTimestamp = uint64(block.timestamp);
     }
 
     /// @notice get pending reward for a given account.
     /// @param account Address of account.
     /// @return pending reward for a given account.
     function getPendingReward(address account) external view returns (uint256 pending) {
-        PoolInfo memory poolInfo = getUpdatedPoolInfo();
-
-        return uint256(int256((balanceOf(account) * poolInfo.emissionPerShare) / 1e18) - _debts[account]);
+        uint256 emission =
+            Math.min((block.timestamp - lastUpdatedTimestamp) * emissionRate, stakeToken.balanceOf(address(this)));
+        uint256 _emissionPerShare = emissionPerShare + (emission * 1e18) / totalSupply();
+        return uint256(int256((balanceOf(account) * _emissionPerShare) / 1e18) - _debts[account]);
     }
 
-    /// @notice get pool info
-    /// @return poolInfo
-    function getPoolInfo() public view returns (PoolInfo memory) {
-        return _poolInfo;
-    }
-
-    /// @notice get pool info updated to the current timestamp
-    function getUpdatedPoolInfo() public view returns (PoolInfo memory) {
-        PoolInfo memory poolInfo = _poolInfo;
-
-        if (block.timestamp <= poolInfo.lastUpdatedTimestamp) return poolInfo;
-
-        uint256 emission = Math.min(
-            (block.timestamp - poolInfo.lastUpdatedTimestamp) * poolInfo.emissionRate,
-            stakeToken.balanceOf(address(this))
-        );
-        poolInfo.emissionPerShare += (emission * 1e18) / totalSupply();
-        poolInfo.lastUpdatedTimestamp = uint64(block.timestamp);
-
-        return poolInfo;
-    }
-
+    /// @dev stake token and get pool token.
+    /// @param amount The amount of token to be staked.
     function mint(uint256 amount) external {
         stakeToken.transferFrom(msg.sender, address(this), amount);
         _mint(msg.sender, amount);
     }
 
+    /// @dev burn pool token and get stake token.
+    /// @param amount The amount of token to be burned.
     function burn(uint256 amount) external {
         stakeToken.transfer(msg.sender, amount);
         _burn(msg.sender, amount);
@@ -73,12 +52,16 @@ contract RewardPool is ERC20 {
 
     /// @notice Update pool info to the current timestamp.
     function updatePool() public {
-        if (block.timestamp > _poolInfo.lastUpdatedTimestamp) {
-            _poolInfo = getUpdatedPoolInfo();
+        if (block.timestamp > lastUpdatedTimestamp) {
+            uint256 emission =
+                Math.min((block.timestamp - lastUpdatedTimestamp) * emissionRate, stakeToken.balanceOf(address(this)));
+            emissionPerShare += (emission * 1e18) / totalSupply();
+            lastUpdatedTimestamp = uint64(block.timestamp);
         }
     }
 
-    function _update(address from, address to, uint256 value) internal override {
+    /// @dev update debt balance on mint, burn, and transfer
+    function _update(address from, address to, uint256 value) internal virtual override {
         super._update(from, to, value);
         if (from != address(0)) {
             _unstake(from, value);
@@ -93,7 +76,7 @@ contract RewardPool is ERC20 {
     /// @param amount The amount of token staked.
     function _stake(address account, uint256 amount) internal {
         updatePool();
-        _debts[account] += int256((amount * _poolInfo.emissionPerShare) / 1e18);
+        _debts[account] += int256((amount * emissionPerShare) / 1e18);
     }
 
     /// @notice Unstake account's token from pool.
@@ -101,7 +84,7 @@ contract RewardPool is ERC20 {
     /// @param amount The amount of token unstaked.
     function _unstake(address account, uint256 amount) internal {
         updatePool();
-        _debts[account] -= int256((amount * _poolInfo.emissionPerShare) / 1e18);
+        _debts[account] -= int256((amount * emissionPerShare) / 1e18);
     }
 
     /// @notice Extract rewards for account.
@@ -109,7 +92,7 @@ contract RewardPool is ERC20 {
     function _extract(address account) internal {
         updatePool();
 
-        int256 accumulatedReward = int256((balanceOf(account) * _poolInfo.emissionPerShare) / 1e18);
+        int256 accumulatedReward = int256((balanceOf(account) * emissionPerShare) / 1e18);
         uint256 pendingReward = uint256(accumulatedReward - _debts[account]);
 
         _debts[account] = int128(accumulatedReward);
@@ -117,16 +100,16 @@ contract RewardPool is ERC20 {
     }
 
     /// @notice Update the given pool's reward rate.
-    /// @param emissionRate New emission rate of the pool.
-    function _setEmissionRate(uint256 emissionRate) internal {
-        _poolInfo.emissionRate = uint64(emissionRate);
+    /// @param emissionRate_ New emission rate of the pool.
+    function _setEmissionRate(uint256 emissionRate_) internal {
+        emissionRate = emissionRate_;
     }
 
     /// @notice Add reward token to the pool
     /// @param account The account that sends the reward.
     /// @param amount The amount of reward to be added.
     function _addReward(address account, uint256 amount) internal {
-        require(_poolInfo.lastUpdatedTimestamp != 0, "Pool does not exist");
+        require(lastUpdatedTimestamp != 0, "Pool does not exist");
 
         rewardToken.transferFrom(account, address(this), amount);
     }
